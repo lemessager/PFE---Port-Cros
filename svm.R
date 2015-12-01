bbc49cda7512de615976b48d9421dff2b24ee86e
Vincent Montalieu <vincent.montalieu@outlook.com>
2015-12-01 16:28:15 +0100

    Début de code pour un SVM. Il manque évidemment les données à analyser, il faudrait qu'on décide ensemble ce qu'on essaye d'apprendre.

---
+++ svm.R
@@ -0,0 +1,14 @@
+# CODE A COMPLETER
+# VOIR QUELLES DONNEES UTILISER
+
+library(e1071)
+
+# Training SVM
+# model <- svm(DATA, LABELS)
+# HINT: subset(x, select = c(columns to choose))
+
+# Prediction
+# prediction = predict(model, MATRIX TO PREDICT)
+
+# Comparison between predicted values and real values
+# table(prediction, LABELS)
