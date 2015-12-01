c9c400c507ed4a08bc0727ae2a9019eb04dcfeb4
朱宇婷 <zhuyuting@zhuyutingdeMacBook-Air.local>
2015-12-01 12:36:38 +0100

    add the codes for showing the bar result

--- analyse_remarque.R
+++ analyse_remarque.R
@@ -15,6 +15,9 @@
 #
 # Editor: ZHU Yuting
 # Version: 2015-11-30
+#
+# Add the result bar
+# Version: 2015-12-01
 
 # Connection with the database
 source("satisfaction_version_complet.R")
@@ -34,5 +37,23 @@ criteria_sat_1 <- apply(satisfait_remarque, 2, cal_pos, 1) # critique positive
 criteria_sat_2 <- apply(satisfait_remarque, 2, cal_pos, 2) # critique negative
 criteria_der_1 <- apply(derangeant_remarque, 2, cal_pos, 1)# trop 
 criteria_der_2 <- apply(derangeant_remarque, 2, cal_pos,2) # pas assez
-criteria_der_3 <- apply(derangeant_remarque, 2, cal_pos,3) # pas assez
+criteria_der_3 <- apply(derangeant_remarque, 2, cal_pos,3) # bien
 
+############### Afficher le resultat #################
+
+library(ggplot2)
+library(ggthemes)
+critere_derangeant <- colnames(derangeant_remarque)
+critere_satisfait <- colnames(satisfait_remarque)
+show_res <- function(sat_der, mat_res){
+dt <- data.frame(sat_der, mat_res)
+  ggplot(dt, aes(x = sat_der, y = mat_res, fill = sat_der, group = factor(1))) + 
+    geom_bar(stat = "identity") +
+    theme_economist()
+}
+
+show_res(sat_der = critere_satisfait, mat_res = criteria_sat_1)  # critique positive
+show_res(sat_der = critere_satisfait, mat_res = criteria_sat_2)  # critique negative
+show_res(sat_der = critere_derangeant, mat_res = criteria_der_1) # trop
+show_res(sat_der = critere_derangeant, mat_res = criteria_der_2) # pas assez
+show_res(sat_der = critere_derangeant, mat_res = criteria_der_3) # bien
