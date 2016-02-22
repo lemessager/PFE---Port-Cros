18a12896f6be4559d56985d9db25d42cbc744a03
Vincent Montalieu <vincent.montalie@outlook.com>
2016-02-22 23:33:08 +0100

    Recuperation du texte sous le plot pour affichage + mise en forme du code

--- port-cros-gui/ui.R
+++ port-cros-gui/ui.R
@@ -1,90 +1,105 @@
 library(shiny)
 #library(datasets)
 
-shinyUI(navbarPage("Port-Cros GUI",
-                   
-                   # TAB 1
-                   tabPanel(
-                     "Visualistion donn??es satisfaction",
-                     
-                     sidebarLayout(
-                       sidebarPanel(
-                         selectInput("sat", "Niveau de satisfaction",
-                                     choices = c("nautique","pieton","total")),
-                         hr(),
-                         helpText("Donn??es des enqu??tes")
-                       ),
-                       mainPanel(
-                         plotOutput("satPlot")
-                       )
-                     )
-                   ),
-                   
-                   # TAB 2
-                   tabPanel(
-                     "Visualistion donn??es satisfaction / frequentation",
-                     
-                     sidebarLayout(
-                       sidebarPanel(
-                         selectInput("nom", "Niveau de satisfaction en fonction de frequentation",
-                                     choices = c("pieton","nautique","total")),
-                         hr(),
-                         helpText("Resultat apres l'analyse")
-                       ),
-                       mainPanel(
-                         plotOutput("nomPlot")
-                       )
-                     )
-                   ),
-                   
-                   
-                   # TAB 3
-                   tabPanel(
-                     "Pr??diction satisfaction",
-                     
-                     sidebarLayout(
-                       sidebarPanel(
-                         sliderInput("day", "Jour", 1, 31, 15),
-                         selectInput("month", "Mois", c("Janvier" = 1,
-                                                        "F??vrier" = 2,
-                                                        "Mars" = 3,
-                                                        "Avril" = 4,
-                                                        "Mai" = 5,
-                                                        "Juin" = 6,
-                                                        "Juillet" = 7,
-                                                        "Ao??t" = 8,
-                                                        "Septembre" = 9,
-                                                        "Octobre" = 10,
-                                                        "Novembre" = 11,
-                                                        "D??cembre" = 12)),
-                         hr(),
-                         actionButton("predict", "Lancer la pr??diction")
-                       ),
-                       mainPanel(
-                         plotOutput("svmPlot")
-                       )
-                     )
-                   ),
-                   
-                   # TAB 4
-                   tabPanel(
-                     "Visualistion donn??es satisfaction / meteo",
-                     
-                     sidebarLayout(
-                       sidebarPanel(
-                         selectInput("met", "Niveau de satisfaction ",
-                                     choices = c("pieton","nautique")),
-                         selectInput("para", "en fonction de", choices = c("pluie", "pluie_neige", "ev_piche", "moyen_temperature", "total")),
-                         checkboxInput("check", strong("x-axe : date"), value = TRUE),
-                         hr(),
-                         helpText("Resultat apres l'analyse")
-                       ),
-
-                       mainPanel(
-                         plotOutput("metPlot")
-                       )
-                     )
-                   )
-                   
-                   
-))
+shinyUI(
+  navbarPage(
+    title = "Dashboard", theme = "main.css",
+    
+    # TAB 1
+    tabPanel(
+      "Visualistion donnees satisfaction",
+      
+      sidebarLayout(
+        sidebarPanel(
+          selectInput(
+            "sat", "Niveau de satisfaction",
+            choices = c("nautique","pieton","total")
+          ),
+          hr(),
+          helpText("Donnees des enquetes")
+        ),
+        mainPanel(plotOutput("satPlot"))
+      )
+    ),
+    
+    # TAB 2
+    tabPanel(
+      "Visualistion donnees satisfaction / frequentation",
+      
+      sidebarLayout(
+        sidebarPanel(
+          selectInput(
+            "nom", "Niveau de satisfaction en fonction de frequentation",
+            choices = c("pieton","nautique","total")
+          ),
+          hr(),
+          helpText("Resultat apres l'analyse")
+        ),
+        mainPanel(plotOutput("nomPlot"))
+      )
+    ),
+    
+    
+    # TAB 3
+    tabPanel(
+      "Prediction satisfaction",
+      
+      sidebarLayout(
+        sidebarPanel(
+          sliderInput("day", "Jour", 1, 31, 15),
+          selectInput(
+            "month", "Mois", c(
+              "Janvier" = 1,
+              "Fevrier" = 2,
+              "Mars" = 3,
+              "Avril" = 4,
+              "Mai" = 5,
+              "Juin" = 6,
+              "Juillet" = 7,
+              "Aout" = 8,
+              "Septembre" = 9,
+              "Octobre" = 10,
+              "Novembre" = 11,
+              "Decembre" = 12
+            )
+          ),
+          hr(),
+          actionButton("predict", "Lancer la prediction")
+        ),
+        mainPanel(
+          plotOutput("svmPlot"),
+          br(),
+          textOutput("dailyMean"),
+          textOutput("monthlyMean"),
+          textOutput("globalMean"),
+          br(),
+          textOutput("maxPass")
+        )
+      )
+    ),
+    
+    # TAB 4
+    tabPanel(
+      "Visualistion donnees satisfaction / meteo",
+      
+      sidebarLayout(
+        sidebarPanel(
+          selectInput("met", "Niveau de satisfaction ",
+                      choices = c("pieton","nautique")),
+          selectInput(
+            "para", "en fonction de", choices = c(
+              "pluie", "pluie_neige", "ev_piche", "moyen_temperature", "total"
+            )
+          ),
+          checkboxInput("check", strong("x-axe : date"), value = TRUE),
+          hr(),
+          helpText("Resultat apres l'analyse")
+        ),
+        
+        mainPanel(plotOutput("metPlot"))
+      )
+    )
+    
+    
+  )
+)
