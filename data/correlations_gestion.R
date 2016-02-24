b43886158beff3b25e7bab6ed59178fcddda5c22
Vincent Montalieu <vincent.montalieu@outlook.com>
2016-02-24 19:22:14 +0100

    MAJ du script de cross correlation pour gestion

--- data/correlations_gestion.R
+++ data/correlations_gestion.R
@@ -1,41 +1,23 @@
-# Connexion avec MySQL
+source("normalize.R")
+source("correlation.R")
 source("readTable.R")
-mysql <- readMyTable()
 
-# On recupere la table
-table <- dbGetQuery(mysql, "SELECT * FROM enquete_gestion")
+mysql = readMyTable()
 
-# On enleve les colonnes inutiles
-table <- table[-3]
-table <- table[-2]
-table <- table[-1]
+gestion <- dbGetQuery(mysql, "SELECT * FROM enquete_gestion")
 
-# On met des 0 a la place des NULL
-table[is.na(table)] <- 0
+gestion <- gestion[-1]
+gestion[is.na(gestion)] <- 0
 
-# On stocke la taille de la table (le nombre de colonnes)
-my_length <- length(table[1,])
+satisfait_gestion_1 <- gestion[c(3)]%%3
+satisfait_gestion_1[satisfait_gestion_1==2] <- -1
 
-# On initialise une matrice de cross correlation
-my_cor <- matrix(numeric(1), nrow = my_length, ncol = my_length)
+satisfait_gestion_2 <- gestion[c(9,10,11,17)]%%4
+satisfait_gestion_2 <- satisfait_gestion_2%%3
+satisfait_gestion_2[satisfait_gestion_2==2] <- -1
 
-# ON REMPLIT LA MATRICE DE CROSS CORRELATION
-for (i in 1:my_length) {
-  for (j in 1:my_length) {
-    my_cor[i,j] <- cor(table[,i], table[,j])
-  }
-}
+satisfait_gestion <- cbind(satisfait_gestion_1, satisfait_gestion_2)
 
-# ON PLOT POUR CHAQUE COLONNE SON VECTEUR DE CROSS CORRELATION
-for(i in 1:my_length) {
-  plot(my_cor[i,], type = "b", ylab = "Correlation", xaxt='n', ann=FALSE)
-  title(colnames(table[i]))
-  axis(1, at=seq(1, my_length, by=1), labels = FALSE)
-  text(seq(1, my_length, by=1), par("usr")[3] - 0.15, cex = 0.8, labels = strtrim(colnames(table), 8), srt = 90, pos = 1, xpd = TRUE)
-  abline(h = 0.7, col = "red")
-  abline(h = - 0.7, col = "red")
-  abline(v = i, col = "blue")
-  grid(30, 30)
-}
+plot_cor(satisfait_gestion)
 
 source("close_db_connections.R")
