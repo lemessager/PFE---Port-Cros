606354b5b6edda31b599481f39e6f3074c890a16
Vincent Montalieu <vincent.montalieu@outlook.com>
2016-02-26 11:56:59 +0100

    On arrête de view les data à chaque fois (utilise pour le debug mais plus maintenant)

--- data/evolution_deb_bat.R
+++ data/evolution_deb_bat.R
@@ -6,9 +6,7 @@ load_data <- function (name) {
   data = data[-1]
   data[,1] = substr(data[,1],6,10)
   colnames(data) = c("date", "nbr")
-  View(data)
   data = aggregate(data[, 2], list(data$date), mean)
-  View(data)
   data = cbind(substr(data[,1],4,5), substr(data[,1],1,2), data[,2])
   table = data.frame(as.numeric(as.character(data[,1])), as.numeric(as.character(data[,2])), as.numeric(as.character(data[,3])))
   colnames(table) = c("jour", "mois", "nbr")
